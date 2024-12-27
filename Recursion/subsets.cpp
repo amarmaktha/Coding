b6df4df7249e8e5f09ab84865b4e7c1b791236a8
M Amar <amar.maktha222@gmail.com>
2024-12-27 17:07:08 +0530

    Update subsets.cpp

--- Recursion/subsets.cpp
+++ Recursion/subsets.cpp
@@ -1,6 +1,9 @@
 // https://leetcode.com/problems/subsets/description/
 // LOGIC: Either include element in array or not include
 // TC: O(2^N).
+// 2nd method
+// Can also be solved by bitmask, suppose there are n elements then there will be 2^n subsets.
+// generate 0 to 2^n bit representation, if 1 include or else if 0 dont include.
   
 class Solution {
 public:
@@ -9,9 +12,9 @@ public:
             AllSubsets.push_back(subset);
             return;
         }
-        func(nums,i+1,AllSubsets, subset); //Dont Include
+        func(nums,i+1,AllSubsets, subset);     //Dont Include
       
-        subset.push_back(nums[i]); //Include
+        subset.push_back(nums[i]);             //Include
         func(nums,i+1,AllSubsets, subset);
 
         return;
